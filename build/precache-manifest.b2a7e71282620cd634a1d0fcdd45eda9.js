937d88ffd31b170b95ced7b0c906ffdbc8e23d47
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-08-06 15:02:33 -0500

    buid

---
+++ build/precache-manifest.b2a7e71282620cd634a1d0fcdd45eda9.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e83205f318954ecd9a24b079e88177e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8b98a5cab206ffeeba1",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "233fb25f4152c292fcf6",
+    "url": "/static/css/3.580f7955.chunk.css"
+  },
+  {
+    "revision": "2063c4b9005e2b5e1398",
+    "url": "/static/css/4.ee73ff20.chunk.css"
+  },
+  {
+    "revision": "9c5cc2b7d7cffda81759",
+    "url": "/static/css/5.f27a9587.chunk.css"
+  },
+  {
+    "revision": "8588f1fe3d6656b94041",
+    "url": "/static/css/main.531e8aaa.chunk.css"
+  },
+  {
+    "revision": "b8b98a5cab206ffeeba1",
+    "url": "/static/js/2.cf78a4d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cf78a4d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233fb25f4152c292fcf6",
+    "url": "/static/js/3.402f75be.chunk.js"
+  },
+  {
+    "revision": "2063c4b9005e2b5e1398",
+    "url": "/static/js/4.d4dae9c6.chunk.js"
+  },
+  {
+    "revision": "9c5cc2b7d7cffda81759",
+    "url": "/static/js/5.13b7484b.chunk.js"
+  },
+  {
+    "revision": "4a82fd4842fd0733767c",
+    "url": "/static/js/6.9bb0de33.chunk.js"
+  },
+  {
+    "revision": "2881d0cd04da8151e42b",
+    "url": "/static/js/7.58e0cc0f.chunk.js"
+  },
+  {
+    "revision": "f53d4dae029e9bafa808",
+    "url": "/static/js/8.9fd2ec43.chunk.js"
+  },
+  {
+    "revision": "8588f1fe3d6656b94041",
+    "url": "/static/js/main.ae8af64e.chunk.js"
+  },
+  {
+    "revision": "f03485780aa479eabf76",
+    "url": "/static/js/runtime-main.c8aba132.js"
+  },
+  {
+    "revision": "963e500cde12be57254cce004c53c015",
+    "url": "/static/media/1-1920x1080.963e500c.png"
+  },
+  {
+    "revision": "908ddef4f6a323e928e67392b89c5729",
+    "url": "/static/media/1-mobile.908ddef4.png"
+  },
+  {
+    "revision": "ee534b0bf1cee3e7e81d93cd7ba91a64",
+    "url": "/static/media/2-1920x1080.ee534b0b.png"
+  },
+  {
+    "revision": "d4014ec8eb7edbc825b05f33711959d9",
+    "url": "/static/media/2-mobile.d4014ec8.png"
+  },
+  {
+    "revision": "6633cc6c1e0db633734445cdc65f6f72",
+    "url": "/static/media/3-1920x1080.6633cc6c.png"
+  },
+  {
+    "revision": "ca2e72c04b0691f197d9194b41fc589e",
+    "url": "/static/media/3-mobile.ca2e72c0.png"
+  },
+  {
+    "revision": "e0563f561c77da8b6ec481f2a916784f",
+    "url": "/static/media/4-1980x1080.e0563f56.png"
+  },
+  {
+    "revision": "b170a6dc0c4b278f338f1000dffe72aa",
+    "url": "/static/media/4-mobile.b170a6dc.png"
+  },
+  {
+    "revision": "233f6214f21a777ae8ab95ad6cb1882a",
+    "url": "/static/media/Candal-Regular.233f6214.ttf"
+  },
+  {
+    "revision": "5d5a1c3671ac57370761afbaf4308204",
+    "url": "/static/media/Hanuman-Bold.5d5a1c36.ttf"
+  },
+  {
+    "revision": "67891d541e5cac9a5c4d468c2ed8cee7",
+    "url": "/static/media/Hanuman-Regular.67891d54.ttf"
+  },
+  {
+    "revision": "3dcf0ae6a78a6a64ae1e3f2406cefa3b",
+    "url": "/static/media/JuliusSansOne-Regular.3dcf0ae6.ttf"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "d9b6ba595b059fc5d48e8f52c30f73b3",
+    "url": "/static/media/Montserrat-BlackItalic.d9b6ba59.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "1b38414956c666bd1df78fe5b9c84756",
+    "url": "/static/media/Montserrat-BoldItalic.1b384149.ttf"
+  },
+  {
+    "revision": "19ba7aa52a78c3896558ac1c0a5fb4c7",
+    "url": "/static/media/Montserrat-ExtraBold.19ba7aa5.ttf"
+  },
+  {
+    "revision": "52a50ca037f2f96fa567404dc3c5bdfb",
+    "url": "/static/media/Montserrat-ExtraBoldItalic.52a50ca0.ttf"
+  },
+  {
+    "revision": "570a244cacd3d78b8c75ac5dd622f537",
+    "url": "/static/media/Montserrat-ExtraLight.570a244c.ttf"
+  },
+  {
+    "revision": "1170df5548b7e238df5fa14b6f1a753e",
+    "url": "/static/media/Montserrat-ExtraLightItalic.1170df55.ttf"
+  },
+  {
+    "revision": "a7063e0c0f0cb546ad45e9e24b27bd3b",
+    "url": "/static/media/Montserrat-Italic.a7063e0c.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "01c4560c9c15069b6700ce7ad2e49a9c",
+    "url": "/static/media/Montserrat-LightItalic.01c4560c.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "40a74702035bf9ef19053c84ce9a58b9",
+    "url": "/static/media/Montserrat-MediumItalic.40a74702.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "83c1ec1f1db9a6416791f7d9d29536f2",
+    "url": "/static/media/Montserrat-SemiBoldItalic.83c1ec1f.ttf"
+  },
+  {
+    "revision": "43dd5b7a3d277362d5e801e5353e3a01",
+    "url": "/static/media/Montserrat-Thin.43dd5b7a.ttf"
+  },
+  {
+    "revision": "3c2b290f95cd5b33c3ead2911064a2ab",
+    "url": "/static/media/Montserrat-ThinItalic.3c2b290f.ttf"
+  },
+  {
+    "revision": "c8d77d3eccf2cf9962956138746e9cd8",
+    "url": "/static/media/PaytoneOne-Regular.c8d77d3e.ttf"
+  },
+  {
+    "revision": "117be28a760dad47011daa2ef0135f51",
+    "url": "/static/media/PlayfairDisplay-Italic-VariableFont_wght.117be28a.ttf"
+  },
+  {
+    "revision": "20e1565dd5c9e47bd241d99c0dd55ad1",
+    "url": "/static/media/PlayfairDisplay-VariableFont_wght.20e1565d.ttf"
+  },
+  {
+    "revision": "56a9676b142056f818bf7fcb8c246350",
+    "url": "/static/media/Vidaloka-Regular.56a9676b.ttf"
+  },
+  {
+    "revision": "c3c1755e5bad6389d0305918a7da8cc3",
+    "url": "/static/media/logo_sm_cut.c3c1755e.png"
+  },
+  {
+    "revision": "565b48677f2e2532b8400b16f33ae448",
+    "url": "/static/media/navlogo.565b4867.png"
+  }
+]);
