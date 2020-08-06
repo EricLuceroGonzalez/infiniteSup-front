29c8d0382d18dbb1ad397bd867d435ee32ddcad4
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-08-06 01:30:02 -0500

    Lazy loading and new build

---
+++ build/precache-manifest.7fbe8ff554d2d037d13cbf4e394f26e9.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02de12f3d564dc4ecd9de127ea7f4ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ff48bd5ea9190652710",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "388ba91ff66206757491",
+    "url": "/static/css/3.580f7955.chunk.css"
+  },
+  {
+    "revision": "d97d73c2744b777d945e",
+    "url": "/static/css/4.ee73ff20.chunk.css"
+  },
+  {
+    "revision": "34257f9a7f006be5c573",
+    "url": "/static/css/5.f27a9587.chunk.css"
+  },
+  {
+    "revision": "0c143936c66a33304b03",
+    "url": "/static/css/6.c75b3d18.chunk.css"
+  },
+  {
+    "revision": "f41211d5a2a3ecefe880",
+    "url": "/static/css/7.c75b3d18.chunk.css"
+  },
+  {
+    "revision": "1a9f9419952501327f14",
+    "url": "/static/css/8.c75b3d18.chunk.css"
+  },
+  {
+    "revision": "c6ff03a540e6ed4a33e5",
+    "url": "/static/css/main.a7cf62b7.chunk.css"
+  },
+  {
+    "revision": "6ff48bd5ea9190652710",
+    "url": "/static/js/2.465130fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.465130fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388ba91ff66206757491",
+    "url": "/static/js/3.6b808007.chunk.js"
+  },
+  {
+    "revision": "d97d73c2744b777d945e",
+    "url": "/static/js/4.50e0bac2.chunk.js"
+  },
+  {
+    "revision": "34257f9a7f006be5c573",
+    "url": "/static/js/5.50a47a29.chunk.js"
+  },
+  {
+    "revision": "0c143936c66a33304b03",
+    "url": "/static/js/6.5d1bb0d4.chunk.js"
+  },
+  {
+    "revision": "f41211d5a2a3ecefe880",
+    "url": "/static/js/7.02ee21b4.chunk.js"
+  },
+  {
+    "revision": "1a9f9419952501327f14",
+    "url": "/static/js/8.984db56b.chunk.js"
+  },
+  {
+    "revision": "c6ff03a540e6ed4a33e5",
+    "url": "/static/js/main.ef0787d4.chunk.js"
+  },
+  {
+    "revision": "91a8a4b78b6ac60047f9",
+    "url": "/static/js/runtime-main.935f522d.js"
+  },
+  {
+    "revision": "45fc78ca94c9936e1e2b9988ee7161db",
+    "url": "/static/media/1-1920x1080.45fc78ca.png"
+  },
+  {
+    "revision": "908ddef4f6a323e928e67392b89c5729",
+    "url": "/static/media/1-mobile.908ddef4.png"
+  },
+  {
+    "revision": "423bca4aa8473d3882a6a6d3702f3005",
+    "url": "/static/media/2-1920x1080.423bca4a.png"
+  },
+  {
+    "revision": "d4014ec8eb7edbc825b05f33711959d9",
+    "url": "/static/media/2-mobile.d4014ec8.png"
+  },
+  {
+    "revision": "60b716b5c549a9bcf5cb0e32c2639700",
+    "url": "/static/media/3-1920x1080.60b716b5.png"
+  },
+  {
+    "revision": "ca2e72c04b0691f197d9194b41fc589e",
+    "url": "/static/media/3-mobile.ca2e72c0.png"
+  },
+  {
+    "revision": "2536274b8090947506fc4f159e435354",
+    "url": "/static/media/4-1980x1080.2536274b.png"
+  },
+  {
+    "revision": "b170a6dc0c4b278f338f1000dffe72aa",
+    "url": "/static/media/4-mobile.b170a6dc.png"
+  },
+  {
+    "revision": "233f6214f21a777ae8ab95ad6cb1882a",
+    "url": "/static/media/Candal-Regular.233f6214.ttf"
+  },
+  {
+    "revision": "5d5a1c3671ac57370761afbaf4308204",
+    "url": "/static/media/Hanuman-Bold.5d5a1c36.ttf"
+  },
+  {
+    "revision": "67891d541e5cac9a5c4d468c2ed8cee7",
+    "url": "/static/media/Hanuman-Regular.67891d54.ttf"
+  },
+  {
+    "revision": "3dcf0ae6a78a6a64ae1e3f2406cefa3b",
+    "url": "/static/media/JuliusSansOne-Regular.3dcf0ae6.ttf"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "d9b6ba595b059fc5d48e8f52c30f73b3",
+    "url": "/static/media/Montserrat-BlackItalic.d9b6ba59.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "1b38414956c666bd1df78fe5b9c84756",
+    "url": "/static/media/Montserrat-BoldItalic.1b384149.ttf"
+  },
+  {
+    "revision": "19ba7aa52a78c3896558ac1c0a5fb4c7",
+    "url": "/static/media/Montserrat-ExtraBold.19ba7aa5.ttf"
+  },
+  {
+    "revision": "52a50ca037f2f96fa567404dc3c5bdfb",
+    "url": "/static/media/Montserrat-ExtraBoldItalic.52a50ca0.ttf"
+  },
+  {
+    "revision": "570a244cacd3d78b8c75ac5dd622f537",
+    "url": "/static/media/Montserrat-ExtraLight.570a244c.ttf"
+  },
+  {
+    "revision": "1170df5548b7e238df5fa14b6f1a753e",
+    "url": "/static/media/Montserrat-ExtraLightItalic.1170df55.ttf"
+  },
+  {
+    "revision": "a7063e0c0f0cb546ad45e9e24b27bd3b",
+    "url": "/static/media/Montserrat-Italic.a7063e0c.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "01c4560c9c15069b6700ce7ad2e49a9c",
+    "url": "/static/media/Montserrat-LightItalic.01c4560c.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "40a74702035bf9ef19053c84ce9a58b9",
+    "url": "/static/media/Montserrat-MediumItalic.40a74702.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "83c1ec1f1db9a6416791f7d9d29536f2",
+    "url": "/static/media/Montserrat-SemiBoldItalic.83c1ec1f.ttf"
+  },
+  {
+    "revision": "43dd5b7a3d277362d5e801e5353e3a01",
+    "url": "/static/media/Montserrat-Thin.43dd5b7a.ttf"
+  },
+  {
+    "revision": "3c2b290f95cd5b33c3ead2911064a2ab",
+    "url": "/static/media/Montserrat-ThinItalic.3c2b290f.ttf"
+  },
+  {
+    "revision": "c8d77d3eccf2cf9962956138746e9cd8",
+    "url": "/static/media/PaytoneOne-Regular.c8d77d3e.ttf"
+  },
+  {
+    "revision": "117be28a760dad47011daa2ef0135f51",
+    "url": "/static/media/PlayfairDisplay-Italic-VariableFont_wght.117be28a.ttf"
+  },
+  {
+    "revision": "20e1565dd5c9e47bd241d99c0dd55ad1",
+    "url": "/static/media/PlayfairDisplay-VariableFont_wght.20e1565d.ttf"
+  },
+  {
+    "revision": "56a9676b142056f818bf7fcb8c246350",
+    "url": "/static/media/Vidaloka-Regular.56a9676b.ttf"
+  },
+  {
+    "revision": "c3c1755e5bad6389d0305918a7da8cc3",
+    "url": "/static/media/logo_sm_cut.c3c1755e.png"
+  },
+  {
+    "revision": "565b48677f2e2532b8400b16f33ae448",
+    "url": "/static/media/navlogo.565b4867.png"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
   BrowserRouter as Router,
@@ -6,20 +6,23 @@ import {
   Redirect,
   Switch,
 } from "react-router-dom";
+
+import { useAuth } from "./shared/hooks/auth-hook";
+import { AuthContext } from "./shared/context/auth-contex";
 import Landing from "./shared/components/App/Home";
 import Footer from "./shared/components/UIElements/Footer";
-import BlogEntry from "./shared/components/App/BlogEntry";
-import AboutUs from "./shared/components/App/AboutUs";
-import ContactUs from "./shared/components/App/ContactUs";
 import Navigation from "./shared/components/Navigation/Navigation";
-import Blog from "./shared/components/App/Blog";
 import ShowProducts from "./shared/components/App/products/showProducts";
 import "./App.css";
 import "./fonts.css";
-import { useAuth } from "./shared/hooks/auth-hook";
-import Auth from "./user/auth";
-import { AuthContext } from "./shared/context/auth-contex";
-import Dashboard from "./user/Dashboard";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner";
+
+const BlogEntry = React.lazy(() => import("./shared/components/App/BlogEntry"));
+const Blog = React.lazy(() => import("./shared/components/App/Blog"));
+const AboutUs = React.lazy(() => import("./shared/components/App/AboutUs"));
+const ContactUs = React.lazy(() => import("./shared/components/App/ContactUs"));
+const Auth = React.lazy(() => import("./user/auth"));
+const Dashboard = React.lazy(() => import("./user/Dashboard"));
 
 function App() {
   const { token, login, logout, userId } = useAuth();
@@ -32,13 +35,13 @@ function App() {
         <Route path="/productos" component={ShowProducts} />
         <Route path="/nosotros" component={AboutUs} />
         <Route path="/contacto" component={ContactUs} />
-        <Route path='/' exact>
-        <Landing/>
+        <Route path="/" exact>
+          <Landing />
         </Route>
         <Route path="/dashboard">
           <Dashboard />
         </Route>
-          <Route path="/dashboard">
+        <Route path="/dashboard">
           <Auth></Auth>
         </Route>
         <Redirect to="/dashboard"></Redirect>
@@ -73,7 +76,17 @@ function App() {
       <Router>
         <div className="App">
           <Navigation />
-          <main className="main">{routes}</main>
+          <main className="main">
+            <Suspense
+              fallback={
+                <div className='center'>
+                  <LoadingSpinner />
+                </div>
+              }
+            >
+              {routes}
+            </Suspense>
+          </main>
           <footer>
             <Footer />
           </footer>
